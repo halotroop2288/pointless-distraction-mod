accf598aad227f6fc199f6539ead70b027b30772
halotroop2288 <halotroop2288@GLaDOS-Mint>
2020-01-28 05:24:08 -0800

    Setup with latest Fabric deps

---
+++ src/main/java/com/github/halotroop2288/pointlessdistraction/DistractionMod.java
@@ -0,0 +1,12 @@
+package com.github.halotroop2288.pointlessdistraction;
+
+import net.fabricmc.api.ModInitializer;
+
+public class DistractionMod implements ModInitializer
+{
+	@Override
+	public void onInitialize()
+	{
+		
+	}
+}
---
+++ src/main/java/com/github/halotroop2288/pointlessdistraction/DistractionModClient.java
@@ -0,0 +1,12 @@
+package com.github.halotroop2288.pointlessdistraction;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class DistractionModClient implements ClientModInitializer
+{
+	@Override
+	public void onInitializeClient()
+	{
+		
+	}	
+}
